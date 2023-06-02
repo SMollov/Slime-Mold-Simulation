5e0d3c3c077d90a1a87af54c932d7dfa7a7511e7
Stefan Mollov <slujeben.stefan@abv.bg>
2023-06-02 14:01:48 +0300

    Added veins effect

--- slime.py
+++ slime.py
@@ -7,15 +7,17 @@ from queue import PriorityQueue
 # Constants
 GRID_WIDTH = 720
 GRID_HEIGHT = 720
-BARRIER_PROB = 0.381966  # Barrier "density"
-UPDATE_FREQUENCY = 400  # Frequency at which the algorithm steps are updated/rendered
+BARRIER_PROB = 0.367713  # Barrier "density" (percolation). Choose from 0 to 0.367713 
+UPDATE_FREQUENCY = 100  # Frequency at which the algorithm steps are updated/rendered
 NEIGHBOR_OFFSETS = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Offsets to get neighboring cells
-VISITED_COLOR = (230, 218, 7)  # Color for visited cells
-DECAY_COLOR = (133, 123, 28)  # Color for old cells
-DECAY_COLOR2 = (22, 26, 10)  # Another color for old cells
+SLIME_COLOR = (171, 160, 70)  # Color for exploring slime
+VEIN_COLOR = (181, 169, 74)  # Color for slime veins
+SLIME_TRAIL_COLOR = (102, 96, 44) # Color for exploring slime trail
+DARK_SLIME_COLOR = (22, 26, 10)  # Color for slime background 
 BUTTONS_COLOR = (110, 24, 24)  # Color for buttons
 BUTTONS_HOVER_COLOR = (60, 0, 15)  # Color for buttons when the mouse is hovering
 FONT = "arial"  # Font used for button text
+
 
 # Map generation
 def generate_barrier_map():
@@ -53,10 +55,14 @@ def dijkstra(screen, barrier_map, start, end):
     queue = PriorityQueue()
     queue.put((0, start))
     visited = np.zeros((GRID_HEIGHT, GRID_WIDTH), dtype=bool)
-    previous = np.full((GRID_HEIGHT, GRID_WIDTH), fill_value=float('inf'), dtype=float)
-    previous[start[1], start[0]] = 0
+    previous = np.full((GRID_HEIGHT, GRID_WIDTH), fill_value=None, dtype=object)
+    costs = np.full((GRID_HEIGHT, GRID_WIDTH), fill_value=float('inf'), dtype=float)
+    costs[start[1], start[0]] = 0
     visited_cells = []
-    
+
+    iteration = 0
+    update_counter = 0
+
     while not queue.empty():
         current_cost, current_cell = queue.get()
         visited[current_cell[1], current_cell[0]] = True
@@ -66,12 +72,13 @@ def dijkstra(screen, barrier_map, start, end):
 
         neighbors = get_neighbors(current_cell, barrier_map)
         for neighbor in neighbors:
-            distance = previous[current_cell[1], current_cell[0]] + 1
-            if distance < previous[neighbor[1], neighbor[0]]:
-                previous[neighbor[1], neighbor[0]] = distance
+            distance = costs[current_cell[1], current_cell[0]] + 1
+            if distance < costs[neighbor[1], neighbor[0]]:
+                costs[neighbor[1], neighbor[0]] = distance
+                previous[neighbor[1], neighbor[0]] = current_cell
                 queue.put((distance, neighbor))
 
-    return visited_cells
+    return visited_cells, previous
 
 def get_neighbors(cell, barrier_map):
     x, y = cell
@@ -119,28 +126,39 @@ def get_user_input(screen, barrier_map):
 
     return start, end
 
-# Path finding process visualization
+# Draw
 def draw_algorithm_steps(screen, barrier_map, start, end):
-    visited_cells = dijkstra(screen, barrier_map, start, end)
+    visited_cells, previous = dijkstra(screen, barrier_map, start, end)
     trail_cells = []
     iteration = 0
-    update_counter = 0
-    update_threshold = UPDATE_FREQUENCY
+
     for cell in visited_cells:
+        # Draw the optimal path to each visited cell
         iteration += 1
-        if len(trail_cells) > (iteration / 11)/3.15:
+        current_cell = cell
+        while current_cell != start:
+            prev_cell = previous[current_cell[1], current_cell[0]]
+            if iteration % UPDATE_FREQUENCY == 0:  # Draw line every n iterations
+                pygame.draw.line(screen, VEIN_COLOR, prev_cell, current_cell, 1) #Veins
+            current_cell = prev_cell
+        trail_iteration = len(trail_cells)
+        if trail_iteration > iteration / 8 :
             decay_cell = trail_cells.pop(0)
-            pygame.draw.rect(screen, VISITED_COLOR, (cell[0], cell[1], 2, 2))
-            pygame.draw.rect(screen, DECAY_COLOR2, (decay_cell[0], decay_cell[1], 2, 2))
-            pygame.draw.rect(screen, DECAY_COLOR, (decay_cell[0], decay_cell[1], 1, 1))
-        else:
-            rect_width = 2
+            pygame.draw.rect(screen, SLIME_COLOR, (cell[0], cell[1], 2, 2)) # Exploring slime
+            pygame.draw.rect(screen, DARK_SLIME_COLOR, (decay_cell[0], decay_cell[1], 2, 2)) #Dark effect
+            pygame.draw.rect(screen, SLIME_TRAIL_COLOR, (decay_cell[0], decay_cell[1], 1, 1)) #Exploring trail effect
+
         trail_cells.append(cell)
 
-        update_counter += 1
-        if update_counter >= update_threshold:
+        if iteration % UPDATE_FREQUENCY == 0:
             pygame.display.update()
-            update_counter = 0
+
+    # Draw the final optimal path
+    current_cell = end
+    while current_cell != start:
+        prev_cell = previous[current_cell[1], current_cell[0]]
+        pygame.draw.line(screen, SLIME_COLOR, prev_cell, current_cell, 2)
+        current_cell = prev_cell
 
 def run_button_action(screen, barrier_map):
     clear_window(screen, barrier_map)
